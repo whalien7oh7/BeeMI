49c37679bfdc476afa18b9d9bb94a28d023c1c08
whalien7oh7 <2015081484@ust-ics.mygbiz.com>
2018-12-06 17:25:47 +0800

    extra commit
    - created food recipe list base layout

--- app/src/main/java/ina/devera/com/beemi/MainActivity.java
+++ app/src/main/java/ina/devera/com/beemi/MainActivity.java
@@ -1,12 +1,9 @@
 package ina.devera.com.beemi;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
